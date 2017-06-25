8ca66e123e70ef787c4c0eddea4da24c24a091bc
yannunes <yannunes@gmail.com>
2017-06-25 17:40:01 +0000

    hashs and index order

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const path = require('path')
 const async = require('async')
 const appDir = process.env.PWD + '/'
 
+const hash = require('./lib/hash/hash.js')
+module.exports.hash = hash
+
 const utils = require('./lib/utils/utils.js')
 module.exports.utils = utils
 
-const hash = require('./lib/hash/hash.js')
-module.exports.hash = hash
 //
 // const connection = require('./lib/connection/connection.js')
 // module.exports.connection = connection
