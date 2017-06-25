e9d3b37194ae27bddc9331adb504da4d9be81e2e
yannunes <yannunes@gmail.com>
2017-06-25 16:47:56 +0000

    index

---
+++ index.js
@@ -0,0 +1,17 @@
+const q = require('q')
+const fs = require('fs')
+const path = require('path')
+const async = require('async')
+const appDir = process.env.PWD + '/'
+
+const utils = require('./lib/utils/utils.js')
+module.exports.utils = utils
+
+const hash = require('./lib/hash/hash.js')
+module.exports.hash = hash
+//
+// const connection = require('./lib/connection/connection.js')
+// module.exports.connection = connection
+//
+// const result = require('./lib/result/result.js')
+// module.exports.result = result
